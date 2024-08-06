67d2f17bbe76668d534d50509d5b3eff19c67f83
naveen9979 <sathyapriyak2020@gmail.com>
2024-08-06 09:40:04 -0700

    Add files via upload

---
+++ true_fa.java
@@ -0,0 +1,13 @@
+public class true_fa{
+    public static void main(String[] args) {
+        boolean attendence=false;
+        
+        if(attendence){
+            System.out.println("I attended class today");
+        }
+        else{
+            System.out.println("I did't attend class today");
+        }
+    }
+}
+
